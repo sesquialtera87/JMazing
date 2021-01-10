3ea088c2b582f3723ff221377f14c73d7ac8d685
Mattia Marelli <75846162+sesquialtera87@users.noreply.github.com>
2021-01-10 17:39:41 +0100

    bux fixed; modified UnionFind data structure
    Reimplemented the UnionFind data structure
    Fixed the cell hash calculation. There was an error in the use of the maze width and height

--- src/jvmMain/kotlin/mth/maze/algo/Kruskal.kt
+++ src/jvmMain/kotlin/mth/maze/algo/Kruskal.kt
@@ -2,7 +2,6 @@ package mth.maze.algo
 
 import mth.maze.Maze
 import mth.maze.MazeRegion
-import mth.maze.UnionFind
 import mth.maze.algo.MazeGenerator.Companion.DX
 import mth.maze.algo.MazeGenerator.Companion.DY
 
@@ -24,73 +23,80 @@ class Kruskal : MazeGenerator {
             val cellHash2 = x * region.width + y
             println(set.differ(cellHash1, cellHash2))
 
-            if (set.differ(cellHash1, cellHash2)) {
-                set.union(set.getNode(cellHash1)!!, set.getNode(cellHash2)!!)
+            if (set.union(cellHash1, cellHash2)) {
                 carvePassage(x, y, direction, maze)
             }
-        } else
-            println("Nothing to do")
+        } else println("Nothing to do")
     }
 
     fun init(mazeRegion: MazeRegion, maze: Maze) {
         this.maze = maze
         this.region = mazeRegion
         this.edges = ArrayList(2 * region.width * region.height)
-        this.set = UnionFind(Array(region.width * region.height) { it })
+        this.set = UnionFind(region.width * region.height)
 
-        for (x in 0 until region.width)
-            for (y in 0 until region.height) {
-                if (y > 0)
-                    edges.add(intArrayOf(x, y, Maze.NORTH))
-                if (x > 0)
-                    edges.add(intArrayOf(x, y, Maze.WEST))
-            }
+        for (x in 0 until region.width) for (y in 0 until region.height) {
+            if (y > 0) edges.add(intArrayOf(x, y, Maze.NORTH))
+            if (x > 0) edges.add(intArrayOf(x, y, Maze.WEST))
+        }
 
         println(edges.size)
     }
 
     override fun generate(region: MazeRegion, maze: Maze): Maze {
         with(region) {
-            // contains the coordinates of the cell and the direction to follow. The coordinates are RELATIVE to the selection
-            val edges: ArrayList<IntArray> = ArrayList(2 * width * height - width * height + 1)
-            val set = UnionFind(Array(width * height) { it })
-
-            println(width)
-            println(height)
+            // contains the coordinates of the cell and the direction to follow. The coordinates are
+            // RELATIVE to the selection
+            val edges: ArrayList<IntArray> = ArrayList(2 * width * height - width - height + 1)
+            val set = UnionFind(width * height)
 
             // initialize the edge collection
-            for (x in 0 until width)
-                for (y in 0 until height) {
-                    if (y > 0)
-                        edges.add(intArrayOf(x, y, Maze.NORTH))
-                    if (x > 0)
-                        edges.add(intArrayOf(x, y, Maze.WEST))
-                }
-
-            println(edges)
+            for (x in 0 until width) for (y in 0 until height) {
+                if (y > 0) edges.add(intArrayOf(x, y, Maze.NORTH))
+                if (x > 0) edges.add(intArrayOf(x, y, Maze.WEST))
+            }
 
             while (edges.isNotEmpty()) {
-                // pick an edge randomly
+                // pick an edge randomly and calculate the coordinates of the other vertex
                 val (x, y, direction) = edges.removeAt(random.nextInt(edges.size))
                 val x1 = x + DX[direction]!!
                 val y1 = y + DY[direction]!!
 
                 // calculate the hashes of the two connected cells
-                val cellHash1 = x1 * width + y1
-                val cellHash2 = x * width + y
+                val cellHash1 = y1 * width + x1
+                val cellHash2 = y * width + x
 
-                if (set.differ(cellHash1, cellHash2)) {
-                    val node1 = set.getNode(cellHash1)
-                    val node2 = set.getNode(cellHash2)
-
-                    if (node1 != null && node2 != null) {
-                        set.union(node1, node2)
+                if (set.union(cellHash1, cellHash2))
                         carvePassage(x + region.x, y + region.y, direction, maze)
-                    }
-                }
             }
         }
 
         return maze
+    }
+
+    class UnionFind(size: Int) {
+
+        val roots = IntArray(size) { it }
+
+        fun find(u: Int): Int {
+            if (roots[u] != u) {
+                val root = find(roots[u])
+                roots[u] = root
+                return root
+            } else return u
+        }
+
+        fun differ(u: Int, v: Int) = find(u) != find(v)
+
+        fun union(u: Int, v: Int): Boolean {
+            val root1 = find(u)
+            val root2 = find(v)
+
+            if (root1 != root2) {
+                roots[root2] = root1
+
+                return true
+            } else return false
+        }
     }
 }
