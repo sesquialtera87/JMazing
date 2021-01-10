9df9b35a0131a01e76261a5fcc9c012b39d67761
Mattia Marelli <75846162+sesquialtera87@users.noreply.github.com>
2021-01-10 18:28:09 +0100

    implemented Sidewinder algorithm

---
+++ src/jvmMain/kotlin/mth/maze/algo/Sidewinder.kt
@@ -0,0 +1,38 @@
+package mth.maze.algo
+
+import mth.maze.*
+
+class Sidewinder : MazeGenerator {
+
+    var carveEastProbability = 0.5
+
+    fun randomChoiche(prob: Double) = random.nextDouble() <= prob
+
+    override fun generate(mazeRegion: MazeRegion, maze: Maze): Maze {
+        with(mazeRegion) {
+            var corridorStart: Int
+
+            // the first row is a single corridor, since we cannot crave north
+            for (i in 0 until width - 1) carvePassage(x + i, 0, Maze.EAST, maze)
+
+            for (j in 1 until height) {
+                corridorStart = 0
+
+                for (i in 0 until width) {
+                    if (i != width - 1 && randomChoiche(carveEastProbability)) {
+                        carvePassage(i + x, j + y, Maze.EAST, maze)
+                    } else {
+                        // choose at random a cell in the corridor and open a passage
+                        // towards north
+                        val corridorLength = i - corridorStart + 1
+                        val northPassagePosition = random.nextInt(corridorLength) + corridorStart
+                        carvePassage(x + northPassagePosition, y + j, Maze.NORTH, maze)
+                        corridorStart = i + 1
+                    }
+                }
+            }
+        }
+
+        return maze
+    }
+}
