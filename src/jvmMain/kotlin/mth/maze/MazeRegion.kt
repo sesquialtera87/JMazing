5c51dd43a3aac0fa327f8e5934e20a2432ba1982
sesquialtera87 <sesquialtera87@gmail.com>
2021-01-11 18:45:50 +0100

    added build method

--- src/jvmMain/kotlin/mth/maze/MazeRegion.kt
+++ src/jvmMain/kotlin/mth/maze/MazeRegion.kt
@@ -1,3 +1,6 @@
 package mth.maze
 
-data class MazeRegion(var x: Int, var y: Int, var width: Int, var height: Int)
+data class MazeRegion(var x: Int, var y: Int, var width: Int, var height: Int) {
+
+    fun build(x1: Int, y1: Int, x2: Int, y2: Int) = MazeRegion(x1, y1, x2 - x1 + 1, y2 - y1 + 1)
+}
